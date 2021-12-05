bab88897e0e9d2744f770ae73b6e5b1683a9b96b
aanjaimi <88238026+aanjaimi@users.noreply.github.com>
2021-12-05 11:17:40 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -11,16 +11,9 @@
 /* ************************************************************************** */
 
 #include<stddef.h>
-//#include<stdio.h>
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
-/*int main()
-{
-	void *s;
-	bzero(s, 5);
-	return(0);
-}*/
